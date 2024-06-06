1ec9cc88ee930948965d35ab95b780269fdc2ae1
Freda <mbordfreda@gmail.com>
2024-06-06 15:58:38 +0000

    math in Javascript

--- public/js/script.js
+++ public/js/script.js
@@ -72,6 +72,86 @@ const tweet1 = {
   }
   
   
+  const tweet1 = {
+    text:'i am learning js today',
+    likes: 5,
+    shares: 2,
+    retweets: 1
+  }
+  
+  const tweet2 = {
+    text:'i am learning arrays today',
+    likes: 8,
+    shares: 3,
+    retweets: 0
+    
+  }
+  const tweets = [];
+  tweets.push(tweet1);
+  tweets.push(tweet2);
+  tweets.length;
+  tweets[1]
+  
+  // dates
+  const today = new Date();
+  today.toString();
+  today.toLocaleDateString();
+  
+  // If/Else
+  if (today.getHours() == 16){ 
+    console.log('class has ended!');
+  } else{
+    console.log('we are false');
+  }
+  
+  // Functions
+  // Defining functions
+  function login(username, password) {
+   if(username == 'Freeda'&& password == '1234') {
+      return 'User is logged in!'
+   } else if (username != 'Freeda') {
+     return 'Invalid username!';
+   }
+     else if (password != '1234') {
+     return 'Invalid password!';
+   }
+     else {
+     return 'invalid username or password!';
+   }
+  }
+  
+  // Invoking functions
+  login('Freeda','1234');
+  
+  //Basic Arithmetric
+  12 + 34;
+  45 - 16;
+  7 * 12;
+  78 / 4;
+  12 % 4;
+  11 % 2
+  15%4
+  
+  // Comparison operator
+  2!== '2'
+  2 === 2
+  4==='4'
+  
+  // Built in Maths functions
+  Math.round(37 / 24);
+  Math.floor(37/ 24);
+  Math.ceil(37 /24)
+  Math.random()
+  Math.max(78, 6 ,45)
+  Math.min(5,16,35,36, 87)
+  
+  
+  //order of operation
+  1*8 + 4 -3
+  
+  //Handling user inputs
+  1200 + Number('250')
+  
   
   
   
